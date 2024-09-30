8a7157ce6e186669ec198b9cf3deceab145c6892
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-09-30 17:26:52 +0300

    deploy

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 import itertools
-from Unidecode import unidecode
+from unidecode import unidecode
 
 
 class Category(models.Model):
