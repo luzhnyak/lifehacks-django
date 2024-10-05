d0f1d303f86bfaef8ef8621897623130994c5247
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-10-05 15:35:58 +0300

    add image to category

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 import itertools
+import os
 from unidecode import unidecode
+from PIL import Image
+
+
+def category_image_upload_to(instance, filename):
+    extension = filename.split('.')[-1]
+    filename = f"{instance.slug}.{extension}"
+    return os.path.join('categories/', filename)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.CharField(max_length=100, unique=True, blank=True)
-    image = models.CharField(max_length=200, blank=True, null=True)
+    image = models.ImageField(
+        upload_to=category_image_upload_to, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
 
     def save(self, *args, **kwargs):
@@ -26,6 +38,74 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def category_image_upload_to(instance, filename):
+    extension = filename.split('.')[-1]
+    filename = f"{instance.slug}.{extension}"
+    return os.path.join('categories/', filename)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True, blank=True)
+    image = models.ImageField(
+        upload_to=category_image_upload_to, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug or self.slug == "":
+            self.slug = slugify(unidecode(self.name))
+            original_slug = self.slug
+            for i in itertools.count(1):
+                if not Category.objects.filter(slug=self.slug).exists():
+                    break
+                self.slug = f'{original_slug}-{i}'
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('category_detail', args=[str(self.slug)])
+
+    def __str__(self):
+        return self.name
+
+
+@receiver(post_save, sender=Category)
+def resize_and_crop_image(sender, instance, **kwargs):
+    if instance.image:
+        img_path = instance.image.path
+        if os.path.exists(img_path):  # Перевіряємо, чи існує файл
+            img = Image.open(img_path)
+
+            # Задаємо потрібні розміри (ширина, висота)
+            target_size = (640, 480)
+
+            # Змінюємо розмір зображення, зберігаючи пропорції
+            img_ratio = img.width / img.height
+            target_ratio = target_size[0] / target_size[1]
+
+            if img_ratio > target_ratio:
+                # Якщо зображення ширше, ніж потрібно, змінюємо висоту, залишаючи ширину пропорційною
+                new_height = target_size[1]
+                new_width = int(new_height * img_ratio)
+            else:
+                # Якщо зображення вище, ніж потрібно, змінюємо ширину, залишаючи висоту пропорційною
+                new_width = target_size[0]
+                new_height = int(new_width / img_ratio)
+
+            # Використовуємо Image.Resampling.LANCZOS для високоякісного зміщення
+            img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
+
+            # Обрізаємо зайві частини для відповідності точному розміру
+            left = (new_width - target_size[0]) / 2
+            top = (new_height - target_size[1]) / 2
+            right = (new_width + target_size[0]) / 2
+            bottom = (new_height + target_size[1]) / 2
+
+            img = img.crop((left, top, right, bottom))
+
+            # Зберігаємо зображення
+            img.save(img_path)
 
 
 class Post(models.Model):
