272a85e5a4a1967b53e1e5cbf8d7ff6c091feb13
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-10-08 00:33:26 +0300

    fix admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Category, Post
 
+
+class BlogAdmin(admin.ModelAdmin):
+    # Відображення полів у списку постів
+    list_display = ('title', 'youtube')
+    list_filter = ('categories',)  # Фільтрація по категорії
+
+
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, BlogAdmin)
