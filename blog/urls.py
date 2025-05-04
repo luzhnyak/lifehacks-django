22d67016b9dad7301e46f3b9254793c25ed5fb89
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2025-05-04 16:25:34 +0300

    clear code

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    # path('', views.index, name='posts'),
+urlpatterns = [    
     path('', views.posts, name='category_detail'),
     path('<str:slug>', views.post, name='post_detail'),
 ]
